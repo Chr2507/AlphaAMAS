b21e6de5844553a7e36a327fe0f8bfdf4425c7cc
Chr2507 <chr.nfs@gmail.com>
2019-04-01 16:03:23 +0200

    Logging

--- run/config.py
+++ run/config.py
@@ -1,7 +1,7 @@
 #### SELF PLAY
-EPISODES = 30
-MCTS_SIMS = 50
-MEMORY_SIZE = 30000
+EPISODES = 5 #30 #10
+MCTS_SIMS = 10 #50
+MEMORY_SIZE = 500#30000
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
